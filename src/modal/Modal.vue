461f21e4f1032096669bc0aac9d8537bb09b6bda
ltslyc <lts.lyc@qq.com>
2021-03-09 22:26:05 +0800

    创建modal组件

---
+++ src/modal/Modal.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Author: lts
+ * @Date: 2021-03-09 22:21:58
+ * @LastEditTime: 2021-03-09 22:24:09
+ * @FilePath: \my-cli-ui\src\modal\Modal.vue
+-->
+<template>
+  <div class="ts-modal">121321231</div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Modal",
+});
+</script>
